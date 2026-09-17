e9c950f46c054804ae6c85b89223dc537e511a02
Rae Crusoe <113061355+mantarae-22@users.noreply.github.com>
2026-09-17 13:05:04 -0700

    Add files via upload

---
+++ mars_home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mars Home Page</title>
+    <!-- link to the external css file -->
+    <link rel="stylesheet" href="mars.css">
+    <!-- link to the JavaScript file; defer to wait to run the JavaScript code until all of the HTML elements have loaded -->
+    <script src="mars.js" defer></script>
+
+</head>
+<body>
+    <!-- Choose a name for your company.
+2. Make a static home page for your company with a .php extension.
+3. Write a short company description on your home page.
+4. Create a CSS file with at least 2 selectors that is linked to from the home page.
+5. Create a JS file that is linked to from the home page. It can be empty for now. -->
+    <header>
+        <h1>Outer Space Adventures</h1>
+    </header>
+    <main>
+        <h2>Travel the galaxy with us!</h2>
+
+        <p>We are the best at getting you from planet to planet.</p>
+    </main>
+</body>
+</html>
